dd3516f3ad8ebc64be8d03041bc61ab16786e1eb
Agus Friastana <friastana@gmail.com>
2020-08-28 10:45:19 +0800

    contract reminder

--- Scheduler/ClassContractReminder.vb
+++ Scheduler/ClassContractReminder.vb
@@ -1,9 +1,10 @@
 ﻿Public Class ClassContractReminder
     Public Shared Sub contract_reminder()
         Dim query As String = "
-            SELECT emp.id_employee, emp.employee_name, emp.employee_position, dep.departement, emp.employee_join_date, emp.start_period, emp.end_period, lvl.grup_penilaian, emp_head.id_employee AS id_employee_head, emp_head.employee_name AS employee_name_head, emp_head.email_external AS email_external_head
+            SELECT emp.id_employee, emp.employee_name, emp.employee_position, sts.employee_status, dep.departement, emp.employee_join_date, emp.start_period, emp.end_period, lvl.grup_penilaian, emp_head.id_employee AS id_employee_head, emp_head.employee_name AS employee_name_head, emp_head.email_external AS email_external_head
             FROM tb_m_employee AS emp
             LEFT JOIN tb_lookup_employee_level AS lvl ON emp.id_employee_level = lvl.id_employee_level
+            LEFT JOIN tb_lookup_employee_status AS sts ON emp.id_employee_status = sts.id_employee_status
             LEFT JOIN tb_m_departement AS dep ON emp.id_departement = dep.id_departement
             LEFT JOIN tb_m_user AS usr_head ON dep.id_user_head = usr_head.id_user
             LEFT JOIN tb_m_employee AS emp_head ON usr_head.id_employee = emp_head.id_employee
@@ -95,7 +96,12 @@
             <br>
             <p style='font: normal 10.00pt/14.25pt Arial; margin: 0;'>Sehubungan dengan kontrak staffnya akan berakhir, maka mohon dapat dibuatkan evaluasi kerja atas nama sbb:</p>
             <br>
-            <p style='font: normal 10.00pt/14.25pt Arial; margin: 0;'>" + data.Rows(i)("employee_name").ToString + " berakhir " + Date.Parse(data.Rows(i)("end_period").ToString).ToString("dd MMMM yyyy") + ".</p>
+            <p style='font: normal 10.00pt/14.25pt Arial; margin: 0;'>Nama Karyawan: " + data.Rows(i)("employee_name").ToString + "</p>
+            <p style='font: normal 10.00pt/14.25pt Arial; margin: 0;'>Posisi: " + data.Rows(i)("employee_position").ToString + "</p>
+            <p style='font: normal 10.00pt/14.25pt Arial; margin: 0;'>Dept: " + data.Rows(i)("departement").ToString + "</p>
+            <p style='font: normal 10.00pt/14.25pt Arial; margin: 0;'>Tgl Mulai Bekerja: " + Date.Parse(data.Rows(i)("employee_join_date").ToString).ToString("dd MMMM yyyy") + "</p>
+            <p style='font: normal 10.00pt/14.25pt Arial; margin: 0;'>Status Karyawan: " + data.Rows(i)("employee_status").ToString + "</p>
+            <p style='font: normal 10.00pt/14.25pt Arial; margin: 0;'>Periode Penilaian: " + Date.Parse(data.Rows(i)("start_period").ToString).ToString("dd MMMM yyyy") + " s/d " + Date.Parse(data.Rows(i)("end_period").ToString).ToString("dd MMMM yyyy") + "</p>
             <br>
             <p style='font: normal 10.00pt/14.25pt Arial; margin: 0;'>Demikian kami sampaikan,  atas perhatian dan kerjasamanya kami ucapkan terimakasih.</p>
             <br>
@@ -105,72 +111,72 @@
             <p style='font: normal 10.00pt/14.25pt Arial; margin: 0;'>Volcom ERP</p>
         "
 
-        Dim path_temp As String = "\\192.168.1.2\dataapp$\emp\document\Form_Penilaian_-_" + data.Rows(i)("employee_name").ToString.Replace(" ", "_") + ".xlsx"
+        'Dim path_temp As String = "\\192.168.1.2\dataapp$\emp\document\Form_Penilaian_-_" + data.Rows(i)("employee_name").ToString.Replace(" ", "_") + ".xlsx"
 
         If data.Rows(i)("grup_penilaian").ToString = "1" Then
             'staff - team leader
             Dim path As String = "\\192.168.1.2\dataapp$\emp\document\Form_Performance_Appraisal_level_Staff_-_Team_Leader.xlsx"
 
-            Dim excel As Microsoft.Office.Interop.Excel.Application = New Microsoft.Office.Interop.Excel.Application
+            'Dim excel As Microsoft.Office.Interop.Excel.Application = New Microsoft.Office.Interop.Excel.Application
 
-            excel.DisplayAlerts = False
+            'excel.DisplayAlerts = False
 
-            excel.Workbooks.Open(path)
+            'excel.Workbooks.Open(path)
 
-            Dim workbook As Microsoft.Office.Interop.Excel.Workbook = excel.ActiveWorkbook
+            'Dim workbook As Microsoft.Office.Interop.Excel.Workbook = excel.ActiveWorkbook
 
-            Dim sheet As Microsoft.Office.Interop.Excel.Worksheet = workbook.Worksheets(1)
+            'Dim sheet As Microsoft.Office.Interop.Excel.Worksheet = workbook.Worksheets(1)
 
-            sheet.Cells(4, 4) = ": " + data.Rows(i)("employee_name").ToString
-            sheet.Cells(5, 4) = ": " + data.Rows(i)("employee_position").ToString
-            sheet.Cells(6, 4) = ": " + data.Rows(i)("departement").ToString
-            sheet.Cells(8, 4) = ": " + Date.Parse(data.Rows(i)("employee_join_date").ToString).ToString("dd MMMM yyyy")
+            'sheet.Cells(4, 4) = ": " + data.Rows(i)("employee_name").ToString
+            'sheet.Cells(5, 4) = ": " + data.Rows(i)("employee_position").ToString
+            'sheet.Cells(6, 4) = ": " + data.Rows(i)("departement").ToString
+            'sheet.Cells(8, 4) = ": " + Date.Parse(data.Rows(i)("employee_join_date").ToString).ToString("dd MMMM yyyy")
 
-            sheet.Cells(7, 10) = ": " + Date.Parse(data.Rows(i)("start_period").ToString).ToString("dd MMMM yyyy") + " s/d " + Date.Parse(data.Rows(i)("end_period").ToString).ToString("dd MMMM yyyy")
+            'sheet.Cells(7, 10) = ": " + Date.Parse(data.Rows(i)("start_period").ToString).ToString("dd MMMM yyyy") + " s/d " + Date.Parse(data.Rows(i)("end_period").ToString).ToString("dd MMMM yyyy")
 
-            workbook.SaveAs(path_temp)
+            'workbook.SaveAs(path_temp)
 
-            workbook.Close()
+            'workbook.Close()
 
-            excel.Quit()
+            'excel.Quit()
 
-            Runtime.InteropServices.Marshal.ReleaseComObject(excel) : excel = Nothing
-            Runtime.InteropServices.Marshal.ReleaseComObject(workbook) : workbook = Nothing
-            Runtime.InteropServices.Marshal.ReleaseComObject(sheet) : sheet = Nothing
+            'Runtime.InteropServices.Marshal.ReleaseComObject(excel) : excel = Nothing
+            'Runtime.InteropServices.Marshal.ReleaseComObject(workbook) : workbook = Nothing
+            'Runtime.InteropServices.Marshal.ReleaseComObject(sheet) : sheet = Nothing
 
-            mail.Attachments.Add(New Net.Mail.Attachment(path_temp))
+            mail.Attachments.Add(New Net.Mail.Attachment(path))
         Else
             'asst. spv - sr. manager
             Dim path As String = "\\192.168.1.2\dataapp$\emp\document\Form_Performance_Appraisal_level_Asst_Spv_-_Sr._Spv.xlsx"
 
-            Dim excel As Microsoft.Office.Interop.Excel.Application = New Microsoft.Office.Interop.Excel.Application
+            'Dim excel As Microsoft.Office.Interop.Excel.Application = New Microsoft.Office.Interop.Excel.Application
 
-            excel.DisplayAlerts = False
+            'excel.DisplayAlerts = False
 
-            excel.Workbooks.Open(path)
+            'excel.Workbooks.Open(path)
 
-            Dim workbook As Microsoft.Office.Interop.Excel.Workbook = excel.ActiveWorkbook
+            'Dim workbook As Microsoft.Office.Interop.Excel.Workbook = excel.ActiveWorkbook
 
-            Dim sheet As Microsoft.Office.Interop.Excel.Worksheet = workbook.Worksheets(1)
+            'Dim sheet As Microsoft.Office.Interop.Excel.Worksheet = workbook.Worksheets(1)
 
-            sheet.Cells(4, 4) = ": " + data.Rows(i)("employee_name").ToString
-            sheet.Cells(5, 4) = ": " + data.Rows(i)("employee_position").ToString
-            sheet.Cells(6, 4) = ": " + data.Rows(i)("departement").ToString
-            sheet.Cells(8, 4) = ": " + Date.Parse(data.Rows(i)("employee_join_date").ToString).ToString("dd MMMM yyyy")
+            'sheet.Cells(4, 4) = ": " + data.Rows(i)("employee_name").ToString
+            'sheet.Cells(5, 4) = ": " + data.Rows(i)("employee_position").ToString
+            'sheet.Cells(6, 4) = ": " + data.Rows(i)("departement").ToString
+            'sheet.Cells(8, 4) = ": " + Date.Parse(data.Rows(i)("employee_join_date").ToString).ToString("dd MMMM yyyy")
 
-            sheet.Cells(7, 10) = ": " + Date.Parse(data.Rows(i)("start_period").ToString).ToString("dd MMMM yyyy") + " s/d " + Date.Parse(data.Rows(i)("end_period").ToString).ToString("dd MMMM yyyy")
+            'sheet.Cells(7, 10) = ": " + Date.Parse(data.Rows(i)("start_period").ToString).ToString("dd MMMM yyyy") + " s/d " + Date.Parse(data.Rows(i)("end_period").ToString).ToString("dd MMMM yyyy")
 
-            workbook.SaveAs(path_temp)
+            'workbook.SaveAs(path_temp)
 
-            workbook.Close()
+            'workbook.Close()
 
-            excel.Quit()
+            'excel.Quit()
 
-            Runtime.InteropServices.Marshal.ReleaseComObject(excel) : excel = Nothing
-            Runtime.InteropServices.Marshal.ReleaseComObject(workbook) : workbook = Nothing
-            Runtime.InteropServices.Marshal.ReleaseComObject(sheet) : sheet = Nothing
+            'Runtime.InteropServices.Marshal.ReleaseComObject(excel) : excel = Nothing
+            'Runtime.InteropServices.Marshal.ReleaseComObject(workbook) : workbook = Nothing
+            'Runtime.InteropServices.Marshal.ReleaseComObject(sheet) : sheet = Nothing
 
-            mail.Attachments.Add(New Net.Mail.Attachment(path_temp))
+            mail.Attachments.Add(New Net.Mail.Attachment(path))
         End If
 
         Dim status As String = ""
@@ -180,7 +186,7 @@
 
             mail.Dispose()
 
-            IO.File.Delete(path_temp)
+            'IO.File.Delete(path_temp)
         Catch ex As Exception
             status = "Failed send contract reminder: " + data.Rows(i)("employee_name").ToString + "; Error: " + ex.Message
 
@@ -194,7 +200,7 @@
         ret_var = ""
 
         Try
-            query = "Select " & field & " FROM tb_opt LIMIT 1"
+            query = "SELECT " & field & " FROM tb_opt LIMIT 1"
             ret_var = execute_query(query, 0, True, "", "", "", "")
         Catch ex As Exception
             ret_var = ""
