fa25e3fcbf11781ec0ccc9d13b8f53823583a257
Satrya Anas <catur.satrya@gmail.com>
2022-03-23 17:55:58 +0800

    prepare

--- Scheduler/ClassSendEmail.vb
+++ Scheduler/ClassSendEmail.vb
@@ -2854,4 +2854,189 @@ AND DATEDIFF(DATE(NOW()),DATE(pl.`complete_date`))>18"
 </table> "
         client.Send(mail)
     End Sub
+
+    Sub send_email_drop_changes()
+        Dim is_ssl = get_setup_field("system_email_is_ssl").ToString
+        Dim client As SmtpClient = New SmtpClient()
+        If is_ssl = "1" Then
+            client.Port = get_setup_field("system_email_ssl_port").ToString
+            client.DeliveryMethod = SmtpDeliveryMethod.Network
+            client.UseDefaultCredentials = False
+            client.Host = get_setup_field("system_email_ssl_server").ToString
+            client.EnableSsl = True
+            client.Credentials = New System.Net.NetworkCredential(get_setup_field("system_email_ssl").ToString, get_setup_field("system_email_ssl_pass").ToString)
+        Else
+            client.Port = get_setup_field("system_email_port").ToString
+            client.DeliveryMethod = SmtpDeliveryMethod.Network
+            client.UseDefaultCredentials = False
+            client.Host = get_setup_field("system_email_server").ToString
+            client.Credentials = New System.Net.NetworkCredential(get_setup_field("system_email").ToString, get_setup_field("system_email_pass").ToString)
+        End If
+
+        Dim subject_mail As String = "DROP & CHANGES NOTIFICATION"
+
+
+        Dim from_mail As MailAddress = New MailAddress("system@volcom.co.id", subject_mail + " - VOLCOM ERP")
+        Dim mail As MailMessage = New MailMessage()
+        mail.From = from_mail
+
+        'Send to
+        Dim query_send_mail As String = "SELECT IF(md.id_user=0,SUBSTRING_INDEX(external_recipient,';',-1),emp.`email_external`) AS email_external, IF(md.id_user=0,SUBSTRING_INDEX(external_recipient,';',1),emp.`employee_name`) AS employee_name
+            FROM tb_mail_to md
+            LEFT JOIN tb_m_user usr ON usr.`id_user`=md.id_user
+            LEFT JOIN tb_m_employee emp ON emp.`id_employee`=usr.`id_employee`
+            WHERE md.report_mark_type='" + report_mark_type + "' AND is_to='1' AND IF(ISNULL(md.id_user),TRUE,IF(IFNULL(emp.id_employee_active,1)=1,TRUE,FALSE))
+            UNION ALL
+            SELECT IF(ISNULL(emp.employee_name),md.email,emp.email_external) AS email_external, IF(ISNULL(emp.employee_name),md.name,emp.employee_name) AS employee_name
+            FROM tb_mail_to_group md
+            LEFT JOIN tb_m_employee emp ON emp.id_employee=md.id_employee
+            WHERE md.report_mark_type='" + report_mark_type + "' AND id_comp_group='" + par1 + "' AND is_to='1' AND IF(ISNULL(md.id_employee),TRUE,IF(IFNULL(emp.id_employee_active,1)=1,TRUE,FALSE))"
+        Dim data_send_mail As DataTable = execute_query(query_send_mail, -1, True, "", "", "", "")
+        For i As Integer = 0 To data_send_mail.Rows.Count - 1
+            Dim to_mail As MailAddress = New MailAddress(data_send_mail.Rows(i)("email_external").ToString, data_send_mail.Rows(i)("employee_name").ToString)
+            mail.To.Add(to_mail)
+        Next
+
+        'Send CC
+        Dim query_send_cc As String = "SELECT IF(md.id_user=0,SUBSTRING_INDEX(external_recipient,';',-1),emp.`email_external`) AS email_external, IF(md.id_user=0,SUBSTRING_INDEX(external_recipient,';',1),emp.`employee_name`) AS employee_name
+            FROM tb_mail_to md
+            LEFT JOIN tb_m_user usr ON usr.`id_user`=md.id_user
+            LEFT JOIN tb_m_employee emp ON emp.`id_employee`=usr.`id_employee`
+            WHERE md.report_mark_type='" + report_mark_type + "' AND is_to='2' AND IF(ISNULL(md.id_user),TRUE,IF(IFNULL(emp.id_employee_active,1)=1,TRUE,FALSE))
+            UNION ALL
+            SELECT IF(ISNULL(emp.employee_name),md.email,emp.email_external) AS email_external, IF(ISNULL(emp.employee_name),md.name,emp.employee_name) AS employee_name
+            FROM tb_mail_to_group md
+            LEFT JOIN tb_m_employee emp ON emp.id_employee=md.id_employee
+            WHERE md.report_mark_type='" + report_mark_type + "' AND id_comp_group='" + par1 + "' AND is_to='2' AND IF(ISNULL(md.id_employee),TRUE,IF(IFNULL(emp.id_employee_active,1)=1,TRUE,FALSE)) "
+        Dim data_send_cc As DataTable = execute_query(query_send_cc, -1, True, "", "", "", "")
+        For i As Integer = 0 To data_send_cc.Rows.Count - 1
+            Dim to_mail As MailAddress = New MailAddress(data_send_cc.Rows(i)("email_external").ToString, data_send_cc.Rows(i)("employee_name").ToString)
+            mail.CC.Add(to_mail)
+        Next
+
+        mail.Subject = subject_mail
+        mail.IsBodyHtml = True
+
+        mail.Body = " <table class='m_1811720018273078822MsoNormalTable' border='0' cellspacing='0' cellpadding='0' width='100%' style='width:100.0%;background:#eeeeee'>
+            <tbody><tr>
+              <td style='padding:30.0pt 30.0pt 30.0pt 30.0pt'>
+              <div align='center'>
+
+              <table class='m_1811720018273078822MsoNormalTable' border='0' cellspacing='0' cellpadding='0' width='600' style='width:6.25in;background:white'>
+               <tbody><tr>
+                <td style='padding:0in 0in 0in 0in'></td>
+               </tr>
+               <tr>
+                <td style='padding:0in 0in 0in 0in'>
+                 <p class='MsoNormal' align='center' style='text-align:center'><a href='http://www.volcom.co.id/' title='Volcom' target='_blank' data-saferedirecturl='https://www.google.com/url?hl=en&amp;q=http://www.volcom.co.id/&amp;source=gmail&amp;ust=1480121870771000&amp;usg=AFQjCNEjXvEZWgDdR-Wlke7nn0fmc1ZUuA'><span style='text-decoration:none'><img border='0' width='180' id='m_1811720018273078822_x0000_i1025' src='https://d3k81ch9hvuctc.cloudfront.net/company/VFgA3P/images/de2b6f13-9275-426d-ae31-640f3dcfc744.jpeg' alt='Volcom' class='CToWUd'></span></a><u></u><u></u></p>
+                </td>
+               </tr>
+               <tr>
+                <td style='padding:0in 0in 0in 0in'></td>
+               </tr>
+               <tr>
+                <td style='padding:0in 0in 0in 0in'>
+                <table class='m_1811720018273078822MsoNormalTable' border='0' cellspacing='0' cellpadding='0' width='600' style='width:6.25in;background:white'>
+                 <tbody><tr>
+                  <td style='padding:0in 0in 0in 0in'>
+
+                  </td>
+                 </tr>
+                </tbody></table>
+
+
+                <p class='MsoNormal' style='background-color:#eff0f1'><span style='display:block;background-color:#eff0f1;height: 5px;'><u></u>&nbsp;<u></u></span></p>
+                <p class='MsoNormal'><span style='display:none'><u></u>&nbsp;<u></u></span></p>
+                
+
+                <!-- start body -->
+                <table width='100%' class='m_1811720018273078822MsoNormalTable' border='0' cellspacing='0' cellpadding='0' style='background:white'>
+                 <tbody>
+                 <tr>
+                  <td style='padding:15.0pt 15.0pt 15.0pt 15.0pt' colspan='3'>
+                  <div>
+                  <p class='MsoNormal' style='line-height:14.25pt'><b><span style='font-family:&quot;Arial&quot;,&quot;sans-serif&quot;;color:#606060;font-size:20px;'>DROP & CHANGES</span></b><span style='font-size:10.0pt;font-family:&quot;Arial&quot;,&quot;sans-serif&quot;;color:#606060;letter-spacing:.4pt'><u></u><u></u></span></p>
+                  <p class='MsoNormal' style='line-height:0.1pt'><b><span style='font-family:&quot;Arial&quot;,&quot;sans-serif&quot;;color:#606060; font-size:12px;'>23 MARCH 2022</span></b><span style='font-size:10.0pt;font-family:&quot;Arial&quot;,&quot;sans-serif&quot;;color:#606060;letter-spacing:.4pt'><u></u><u></u></span></p>
+                  </div>
+                  </td>
+                 </tr>
+         
+                 <tr>
+                  <td style='padding:15.0pt 15.0pt 15.0pt 15.0pt' colspan='3'>
+                     <table width='100%' class='m_1811720018273078822MsoNormalTable' border='1' cellspacing='0' cellpadding='5' style='background:white; font-size: 12px; font-family:&quot;Arial&quot;,&quot;sans-serif&quot;;color:#000000; margin-top: -10px;'>
+                    <tr style='background-color:black; font-size: 12px; font-family:&quot;Arial&quot;,&quot;sans-serif&quot;;color:#ffffff'>
+                      <th>No</th>
+                      <th>Season</th>
+                      <th>Status</th>
+                      <th>Code</th>
+                      <th>Class</th>
+                      <th>Description</th>
+                      <th>Silhouette</th>
+                      <th>Color</th>
+                    </tr>
+      
+                    <tr>
+                      <td>1</td>
+                      <td>S2 22</td>
+                      <td>DROP</td>
+                      <td>340100127</td>
+                      <td>MSF</td>
+                      <td>MSF SHATTER BLK</td>
+                      <td>TEES</td>
+                      <td>BLK</td>
+                    </tr> 
+
+                    <tr>
+                      <td>2</td>
+                      <td>S2 22</td>
+                      <td>MOVE TO S3 22</td>
+                      <td>340100127</td>
+                      <td>MSF</td>
+                      <td>MSF SHATTER BLK</td>
+                      <td>TEES</td>
+                      <td>BLK</td>
+                    </tr> 
+                  </table>
+                  </td>
+
+                 </tr>
+
+         
+          <tr>
+                  <td style='padding:15.0pt 15.0pt 15.0pt 15.0pt' colspan='3'>
+                  <div>
+                  <p class='MsoNormal' style='line-height:14.25pt'><span style='font-size:10.0pt;font-family:&quot;Arial&quot;,&quot;sans-serif&quot;;color:#606060;letter-spacing:.4pt'>Thank you<br /><b>Volcom ERP</b><u></u><u></u></span></p>
+
+                  </div>
+                  </td>
+                 </tr>
+                </tbody>
+              </table>
+              <!-- end body -->
+
+
+                <p class='MsoNormal' style='background-color:#eff0f1'><span style='display:block;height: 10px;'><u></u>&nbsp;<u></u></span></p>
+                <p class='MsoNormal'><span style='display:none'><u></u>&nbsp;<u></u></span></p>
+                <div align='center'>
+                
+
+                <table class='m_1811720018273078822MsoNormalTable' border='0' cellspacing='0' cellpadding='0' style='background:white'>
+                 <tbody><tr>
+                  <td style='padding:6.0pt 6.0pt 6.0pt 6.0pt;text-align:center;'>
+                    <span style='text-align:center;font-size:7.0pt;font-family:&quot;Arial&quot;,&quot;sans-serif&quot;;color:#a0a0a0;letter-spacing:.4pt;'>This email send directly from system. Do not reply.</b><u></u><u></u></span>
+                      <p class='MsoNormal' align='center' style='margin-bottom:12.0pt;text-align:center;padding-top:0px;'><br></p>
+                  </td>
+                 </tr>
+                </tbody></table>
+                </div>
+                </td>
+               </tr>
+              </tbody></table>
+              </div>
+              </td>
+             </tr>
+            </tbody>
+        </table> "
+        client.Send(mail)
+    End Sub
 End Class
